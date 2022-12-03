28b2cba6116b14a91c89e5b04a9ba264d6586350
Jonathan Nilsson <jonathan@voysys.se>
2022-12-03 16:40:29 +0100

    Ai is awesome

---
+++ src/draw.rs
@@ -0,0 +1,95 @@
+use glam::{ivec2, IVec2};
+
+fn draw_line(buffer: &mut [u8], size: IVec2, start: IVec2, end: IVec2, color: u32) {
+    let mut x = start.x;
+    let mut y = start.y;
+
+    let dx = (end.x - start.x).abs();
+    let dy = (end.y - start.y).abs();
+
+    let sx = if start.x < end.x { 1 } else { -1 };
+    let sy = if start.y < end.y { 1 } else { -1 };
+
+    let mut err = if dx > dy { dx } else { -dy } / 2;
+    let mut err2;
+
+    loop {
+        if x >= 0 && x < size.x && y >= 0 && y < size.y {
+            let offset = (y * size.x + x) * 4;
+            buffer[offset as usize] = (color >> 24) as u8;
+            buffer[offset as usize + 1] = (color >> 16) as u8;
+            buffer[offset as usize + 2] = (color >> 8) as u8;
+            buffer[offset as usize + 3] = color as u8;
+        }
+
+        if x == end.x && y == end.y {
+            break;
+        }
+
+        err2 = err;
+
+        if err2 > -dx {
+            err -= dy;
+            x += sx;
+        }
+        if err2 < dy {
+            err += dx;
+            y += sy;
+        }
+    }
+}
+
+fn draw_filled_circle(buffer: &mut [u8], size: IVec2, pos: IVec2, radius: i32, color: u32) {
+    let mut x = radius;
+    let mut y = 0;
+    let mut err = 0;
+
+    while x >= y {
+        draw_line(
+            buffer,
+            size,
+            IVec2::new(pos.x + x, pos.y + y),
+            IVec2::new(pos.x - x, pos.y - y),
+            color,
+        );
+        draw_line(
+            buffer,
+            size,
+            IVec2::new(pos.x + y, pos.y + x),
+            IVec2::new(pos.x - y, pos.y - x),
+            color,
+        );
+        draw_line(
+            buffer,
+            size,
+            IVec2::new(pos.x - x, pos.y + y),
+            IVec2::new(pos.x + x, pos.y - y),
+            color,
+        );
+        draw_line(
+            buffer,
+            size,
+            IVec2::new(pos.x - y, pos.y + x),
+            IVec2::new(pos.x + y, pos.y - x),
+            color,
+        );
+
+        y += 1;
+        err += 1 + 2 * y;
+        if 2 * (err - x) + 1 > 0 {
+            x -= 1;
+            err += 1 - 2 * x;
+        }
+    }
+}
+
+pub fn draw(buffer: &mut [u8], size: IVec2) {
+    draw_line(buffer, size, ivec2(0, 0), ivec2(100, 100), 0xff0000ff);
+    draw_line(buffer, size, ivec2(0, 50), ivec2(100, 50), 0x00ff00ff);
+    draw_line(buffer, size, ivec2(50, 0), ivec2(50, 100), 0x0000ffff);
+
+    draw_line(buffer, size, ivec2(0, 0), ivec2(50, 100), 0xffff00ff);
+    draw_line(buffer, size, ivec2(0, 0), ivec2(100, 50), 0xffff00ff);
+
+    draw_filled_circle(buffer, size, ivec2(50, 50), 10, 0xff00ffff);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,59 @@
-static INPUT: &str = "A Y
-B X
-C Z";
+use glam::ivec2;
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
 
-pub fn a(input: &str) -> i32 {
-    input
-        .lines()
-        .map(|line| match line {
-            "A X" => 1 + 3,
-            "A Y" => 2 + 6,
-            "A Z" => 3,
-            "B X" => 1,
-            "B Y" => 2 + 3,
-            "B Z" => 3 + 6,
-            "C X" => 1 + 6,
-            "C Y" => 2,
-            "C Z" => 3 + 3,
-            _ => panic!("{line}"),
-        })
-        .sum::<i32>()
-}
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
 
-pub fn main() {
-    let res = a(INPUT);
+mod draw;
 
-    println!("Res: {}", res);
-}
+fn main() -> Result<(), Error> {
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Hello Pixels")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
 
-#[test]
-fn test_a() {
-    assert_eq!(a(INPUT), 15);
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            draw::draw(pixels.get_frame_mut(), ivec2(WIDTH as _, HEIGHT as _));
+            if pixels
+                .render()
+                .map_err(|e| println!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        if input.update(&event) {
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+
+            window.request_redraw();
+        }
+    });
 }
